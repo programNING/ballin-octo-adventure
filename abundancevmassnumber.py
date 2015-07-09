0a00ae16059f2f202acc6325f77455864b2adf3d
sning-SURF2015 <sning-SURF2015@users.noreply.github.com>
2015-07-09 11:38:26 -0700

    more generalized abvm.py
    trying to turn this into an actual module. now has two functions, one of which will take a .h5 file generated from skynet and output a dictionary of mass number | abundance. the other will take an input of the list of dictionaries generated (if more than one dataset is being plotted) as well as a list of legend labels and create a plot. THINGS TO WORK ON: could benefit from a main function. legend might require order dictionaries = order of legend labels in order to correctly label. is that ok? dude idk how to fix that.

--- abundancevmassnumber.py
+++ abundancevmassnumber.py
@@ -1,48 +1,60 @@
 import numpy as np
 import h5py as h5
+import matplotlib as mpl
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatch
+import random as r
+import palettable as pltable
 
-infile = h5.File('SkyNet_r-process_python.h5', 'r')
+def abundancevmass(fil):
+	infile = h5.File(fil, 'r')
 
-abundance = infile['/Y']
-nucleons = infile['/A']
-
-
-# Takes only the final abundance. A (1, 7841) array.
-Y = np.array(abundance[-1])
-
-# A (7841, 1) array.
-A = np.array(nucleons)
+	abundance = infile['/Y']
+	nucleons = infile['/A']
 
 
-infile.close()
+	# Takes only the final abundance. A (1, 7841) array.
+	Y = np.array(abundance[-1])
 
-# Create a dictionary for mass number | abundance
-
-yzdict = {}
-
-# For loop iterating over nuclides--all with the same mass number
-# need to have their abundances added together.
-
-for i in range(len(A)):
-	if A[i] in yzdict:
-		yzdict[A[i]] += Y[i]
-	else:
-		yzdict[A[i]] = Y[i]
+	# A (7841, 1) array.
+	A = np.array(nucleons)
 
 
-# Plot that stuff!!!
+	infile.close()
 
-y_vs_z = plt.semilogy(yzdict.keys(), yzdict.values(), "b", lw = 1)
+	# Create a dictionary for mass number | abundance
 
-plt.xlim(0, max(A))
-plt.ylim(10**-13, max(Y))
+	yzdict = {}
 
-plt.xlabel("Mass Number (A)")
-plt.ylabel("Total Abundance (Y)")
+	# For loop iterating over nuclides--all with the same mass number
+	# need to have their abundances added together.
+	print 'Length of A: ', len(A)
+	for i in range(len(A)):
+		if A[i] in yzdict:
+			yzdict[A[i]] += Y[i]
+		else:
+			yzdict[A[i]] = Y[i]
+
+	print min(yzdict, key=yzdict.get)
+	print 'Length of yzdict:', len(yzdict)
+	return yzdict
 
 
-plt.show()
-plt.savefig("Abundance_vs_Mass.pdf")
+# Plot that shit
 
+# More generalized, requires more user input. BLAH so lazy
+def plot_many(lst, legend_lst):
+	cntr = 0
+#	Ye_amnt = [0.01, 0.05, 0.10, 0.20, 0.30, 0.35, 0.40]
+	fig = plt.figure()
+	ax1 = plt.subplot(111)
+	ax1.set_color_cycle(pltable.colorbrewer.qualitative.Dark2_8.mpl_colors)
+	for i in lst:
+		meow = plt.semilogy(i.keys(), i.values(), \
+			label = legend_lst[cntr])
+		cntr += 1
+	plt.legend(loc=0)
+	ax1.set_xlabel("Mass Number (A)")
+	ax1.set_ylabel("Total Abundance (Y)")
+
+
+	plt.show()
